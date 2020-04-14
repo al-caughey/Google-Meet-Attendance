2c00ee854a478b085a2ab0839569d7d37298e6cb
al-caughey <55103652+al-caughey@users.noreply.github.com>
2020-04-13 20:51:20 -0400

    Initial upload of the stand-alone script

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Google-Meet-Attendance
-An extension that allows you to input a list of invitees and then record who actually attendend
+# Introduction and acknowledgements
+
+This extension allows you to input a list of invitees to a Google Meet and then updates the list to indicate who actually attended.  It was initially inspired by, Chris Gamble's Userscript(https://greasyfork.org/en/scripts/397862-google-meet-grid-view) and the related extension by Ryan Meyers (https://github.com/stgeorgesepiscopal/google-meet-grid-view-extension).  Originally, I incorporated the attendance into Chris' userscript but subsequently, I have re-written it as an entirely separate extension.  It is intended the help teachers, like myself, who are rapidy transitioning to the new reality of online classes.  
+
+This extension does not extra permissions except access to meet.google.com. The list of invitees is retained in a LocalStorage variable but none of that information is transmitted or shared elsewhere by the extension.  All of the source code can be viewed at [public repository](https://github.com/al-caughey/Google-Meet-Attendance).
+
+Send kudos and constructive feedback to allan.caughey@ocdsb.ca.  I will do my best to reply to feature requests or support issues but please understand that my students (and home-life) come first.  
+
+# Cloning / building
+
+NB - I am not a github expert (in fact nowhere near close to that!)... so you might need to go elsewhere if you need help with cloning and building your own variant of this script.
+This repo uses submodules to keep pulling the latest userscript... so you'll want to do `git clone --recursive` if you're installing the script locally.
+
+[stolen verbatim from Ryan Meyers repo] To build the script yourself, just clone the repo, go to the directory and use `zip -r packed.zip src` that will generate the zip file you need to upload to the Chrome Web Store (I think it costs like $5 one time to become a developer on there). Oh, and be sure to adjust the variables in `manifest.json` to reflect your own information.
+
---
+++ src/js/attendance.js
@@ -0,0 +1,170 @@
+// ==UserScript==
+// @name         Google Meet Grid View & Attendance
+// @namespace    https://openuserjs.org/users/Al_Caughey
+// @version      0.1.2
+// @description  registers whether or not invitees actually joined a Meet
+// @author       Al Caughey
+// @include      https://meet.google.com/*
+// @grant        none
+// @license      MIT
+// @run-at       document-idle
+// ==/UserScript==
+
+;(function() {
+	
+	//NB - this is still a very early prototype of this extension...  
+	//Please send feedback to allan.caughey@ocdsb.ca
+
+	//TO-DO - internationalization?!? (or is that a little optimistic)
+	//br-->presenting: apresentando | presentation: Apresentação 
+	//fr-->presenting: présentez | presentation: Présentation  
+	const translations={
+		en:{ presenting:'presenting', presentation:'presentation'},
+		br:{ presenting:'apresentando', presentation:'apresentação'},
+		fr:{ presenting:'présentez', presentation:'présentation'},
+	}
+	
+	// return one of the translated strings based on language
+	function getString(lang, str){
+		console.log('getString', lang, str, translations[lang][str])
+		if(!translations[lang]) lang='en'
+		return translations[lang][str]
+	}
+	
+	// TO-DO - detect the language
+	const lang='en'
+	let presenting=getString(lang, 'presenting')
+	let presentation=getString(lang, 'presentation')
+	
+	// simple function that waits until a specific element exists in the DOM...
+	// (adapted from Stack Overflow
+	function waitForElement(elementPath, callBack){
+		window.setTimeout(function(){
+			if($(elementPath).length){
+				callBack(elementPath, $(elementPath));
+			}
+			else{
+				waitForElement(elementPath, callBack);
+			}
+		},100)
+	}
+	
+	// update the LocalStorage variable when the list has changed
+	function listChanged(){
+		console.log('listChanged', $("#Invited-List")[0], $("#Attendance-div")[0])
+		if (!$("#Invited-List")[0]) return
+		let ct=($("#Invited-List")[0].value).trim()
+		if(ct===''){
+			document.getElementById('cl-chk').classList.add('hidden')
+		}
+		else{
+			document.getElementById('cl-chk').classList.remove('hidden')
+		}
+		localStorage.setItem('Meet-Invited-List',ct)
+	}
+
+	// remove all preceding ✔|? from the list of names in the textarea
+	function clearPresent(){
+		console.log('clearPresent')
+		var textarea = document.getElementById("Invited-List");
+		let ct=textarea.value
+		textarea.value=ct.replace(/[✔\?] /g,'')
+		localStorage.setItem('Meet-Invited-List',ct)
+	}
+
+	// clear the textarea
+	function clearList(){
+		console.log('clearList')
+		document.getElementById("Invited-List").value='';
+		localStorage.setItem('Meet-Invited-List','')
+		$("#Attendance-div")[0].addClass('empty')
+	}
+
+	// update the attendance status of the invitees
+	function checkParticipants(){
+		console.log('checkParticipants')
+		let participants=document.querySelectorAll('[data-requested-participant-id]')
+		if(!participants) return
+		
+		var tta = $("#Invited-List")[0];
+		let tal=tta.value
+		let tallc=tal.toLowerCase()
+		let changed=false
+		for (let aa of participants){
+			let pn=aa.innerText
+			let lc=pn.toLowerCase()
+			
+			// TO-DO: I'm 99% certain the line below *will* require updates for non-English renderings
+			if(lc=='you' || lc.indexOf(presenting)>=0 || lc.indexOf(presentation)>=0) continue
+			
+			if(tallc.indexOf(lc)==-1){
+				console.log(pn + ' joined (unexpectedly)')
+				tal+='\n? '+pn + ' (not invited?!?)'
+				changed=true
+			}
+			else if(tallc.indexOf('? '+ lc)>=0){
+				continue // already uninvited
+			}
+			else if(tallc.indexOf('✔ '+ lc)>=0){
+				continue // already marked present
+			}
+			 else if(tallc.indexOf('✔ '+ lc)==-1){
+				const pattern=new RegExp(pn, 'i')
+				console.log(pn + ' joined (as expected)')
+				tal=tal.replace(pattern,'✔ '+pn)
+				changed=true
+			}
+			else{
+				console.log('WTF - ' + pn)
+			}
+		}
+		//if the list changed, a littlehousekeeping and save the changes
+		if (changed) {
+			tta.value=tal.trim().replace('✔ ✔ ','✔ ')
+			listChanged()
+		}
+	}
+	  
+	//setup - the attendance div and `buttons`
+	const atd = $("<div />").attr('id','Attendance-div').addClass('empty');
+	const atp =  $("<p />")
+	const s1 = $("<span />").attr('id','cl-hdr').text('Class List')
+	const s2 = $("<span />").attr('id','cl-ccl').attr('title','Clear attendance checklist').text('[-]')
+	const s3= $("<span />").attr('id','cl-clr').attr('title','Clear list').text('[x]')
+	const s4= $("<span />").attr('id','cl-chk').attr('title','Check Attendance list').text('[✔]').addClass('hidden')
+	const atta =  $("<textarea />").attr('id','Invited-List').attr('placeholder','Paste your class list or list of invitees here...')
+	
+	// set the value of the textarea to the value of the LocalStorage variable 
+	// BTW - admin policies for some schoolboards may clear LS variables on exit
+	if(!!localStorage.getItem('Meet-Invited-List')){
+		atta.val(localStorage.getItem('Meet-Invited-List'))
+		atd.removeClass('empty')
+	}
+	
+	//append the elements
+	atp.append(s1).append(s2).append(s3).append(s4)
+	atd.append(atp).append(atta)
+	atd.appendTo($('body'))
+	
+	//set the behaviours
+	$('#Attendance-div').draggable() // so it can be moved about on the screen
+	$('#Invited-List').change(listChanged)
+	$('#cl-ccl').click(clearPresent)
+	$('#cl-clr').click(clearList)
+	$('#cl-chk').click(checkParticipants)
+	
+	// Create an observer instance to look for changes on the page (detect new participants)
+	var observer = new MutationObserver(function( mutations ) {
+		checkParticipants()
+	});
+	
+	//wait until the DOM element holding the participant info is available
+	waitForElement("[data-allocation-index]",function(){
+		document.getElementById('cl-chk').classList.remove('hidden')
+		checkParticipants()
+		
+		// watch for changes (adding new participants to the Meet)
+		observer.observe(document.body, {childList:true, attributes:false, subtree:true, characterData:true});
+	});
+
+})()
